012e24128b09f1bdddd8d24eaa721e45870d8cf4
VIPIN YADAV <yadavvipinysy083@gmail.com>
2025-03-28 19:26:34 +0530

    improve texts writting

--- src/pages/landing-page/DreamJob.tsx
+++ src/pages/landing-page/DreamJob.tsx
@@ -4,13 +4,13 @@ const DreamJob = () => {
     return (
         <div className='flex  items-center px-20'>
 
-            <div className='flex flex-col w-[45%]'>
+            <div className='flex flex-col w-[45%] text-mine-shaft-100'>
 
-                <div className='text-6xl font-bold text-mine-shaft-100 [&>span]:text-bright-sun-400'>
+                <div className='text-6xl font-bold  [&>span]:text-bright-sun-400'>
                     Find Your <span >Dream Job</span> With Us
                 </div>
 
-                <div>
+                <div className='text-lg'>
                     Good Life Begins With Good Company,Start Explore Thousand Of Jobs In One Place
                 </div>
 
