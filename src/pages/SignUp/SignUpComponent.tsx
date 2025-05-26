8b1660568aa99a504ed085f5114d0ed61fe1b2d3
VIPIN YADAV <yadavvipinysy083@gmail.com>
2025-05-26 23:07:33 +0530

    created services for user register and login and added account type radio button in signup page

--- src/pages/SignUp/SignUpComponent.tsx
+++ src/pages/SignUp/SignUpComponent.tsx
@@ -1,10 +1,11 @@
-import { Anchor, Button, Checkbox, PasswordInput, TextInput } from '@mantine/core'
+import { Anchor, Button, Checkbox, PasswordInput, Radio, TextInput } from '@mantine/core'
 import { IconAt, IconLock } from '@tabler/icons-react'
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom';
 
 const SignUpComponent = () => {
     const [checked, setChecked] = useState(false);
+    const [role, setRole] = useState<"APPLICANT" | "EMPLOYER">("APPLICANT");
 
     return (
         <div className='w-1/2 px-20 flex flex-col justify-center items-center gap-5'>
@@ -63,6 +64,18 @@ const SignUpComponent = () => {
                 icon={<IconLock className='text-bright-sun-400' />}
                 withAsterisk
             />
+            <div className='flex justify-start w-3/4 gap-5'>
+                <Radio.Group
+                    name="favoriteFramework"
+                    label="Select your Account Type"
+                    value={role}
+                    onChange={(value) => setRole(value as "APPLICANT" | "EMPLOYER")}
+                    withAsterisk
+                >
+                    <Radio className='px-4 py-4 hover:bg-mine-shaft-900 has-[:checked]:bg-bright-sun-400/5 has-[:checked]:border-bright-sun-400 border border-mine-shaft-800 rounded-lg' value="APPLICANT" label="APPLICANT" />
+                    <Radio className='px-4 py-4 hover:bg-mine-shaft-900 has-[:checked]:bg-bright-sun-400/5 has-[:checked]:border-bright-sun-400 border border-mine-shaft-800 rounded-lg' value="EMPLOYER" label="EMPLOYER" />
+                </Radio.Group>
+            </div>
             <div className='flex justify-start w-3/4'>
                 <Checkbox
                     checked={checked} onChange={(event) => setChecked(event.currentTarget.checked)}
---
+++ src/services/UserServices.tsx
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+const BASE_URL = "http://localhost:8080/users/";
+
+const registerUser = async (userData: any) => {
+    return axios.post(BASE_URL + "register", userData)
+        .then(response => response.data)
+        .catch(error => {
+            console.error("There was an error registering the user!", error);
+            throw error;
+        });
+}
+const login = async (userData: any) => {
+    return axios.post(BASE_URL + "login", userData)
+        .then(response => response.data)
+        .catch(error => {
+            console.error("There was an error registering the user!", error);
+            throw error;
+        });
+}
+
+export { registerUser, login };
