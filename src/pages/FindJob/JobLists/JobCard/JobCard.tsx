cadabe2719a504515018705c5364d87ad1c5eb03
VIPIN YADAV <yadavvipinysy083@gmail.com>
2025-06-30 07:50:29 +0530

    added formatDateToDayFromCurrentDate utility function and redered how time ago job is posted

--- src/pages/FindJob/JobLists/JobCard/JobCard.tsx
+++ src/pages/FindJob/JobLists/JobCard/JobCard.tsx
@@ -2,6 +2,7 @@ import { IconBookmark, IconClockHour3, IconCurrencyRupee, IconPointFilled } from
 import React from 'react'
 import { Divider, Text } from '@mantine/core'
 import { Link } from 'react-router-dom'
+import { formatDateToDayFromCurrentDate } from '../../../../services/Utilities/Utilities'
 const JobCard = (props: any) => {
     return (
         <Link to={"/job-profile"} className='cursor-pointer bg-mine-shaft-900 w-80 p-4 rounded-xl flex flex-col gap-3 hover:shadow-[0_0_15px_5px_rgba(255,223,0,0.5)] !hover:shadow-bright-sun-400 transition-all duration-500 ease-in-out'>
@@ -11,8 +12,12 @@ const JobCard = (props: any) => {
                         <img className='h-7' src={`/Icons/${props.company}.png`} alt="hi" />
                     </div>
                     <div className='flex flex-col gap-1'>
-                        <div className=' font-semibold tracking-wide'>{props.jobTitle}</div>
-                        <div className='text-xs text-mine-shaft-300'>{props.company} <IconPointFilled width={16} height={16} className='inline-block text-bright-sun-400' /> {props.applicants ? props.applicants.length : 0} applicants</div>
+                        <div className=' font-semibold tracking-wide'>
+                            {props.jobTitle}
+                        </div>
+                        <div className='text-xs text-mine-shaft-300'>
+                            {props.company}
+                            <IconPointFilled width={16} height={16} className='inline-block text-bright-sun-400' /> {props.applicants ? props.applicants.length : 0} applicants</div>
                     </div>
                 </div>
                 <IconBookmark className='text-mine-shaft-300 hover:fill-mine-shaft-300 hover:stroke-mine-shaft-300 transition-all duration-200' stroke={1.5} />
@@ -28,8 +33,10 @@ const JobCard = (props: any) => {
             <Divider mr="xs" color='mine-shaft-700' />
 
             <div className='flex justify-between text-xs text-mine-shaft-400 items-center'>
-                <div className='text-mine-shaft-200 [text-shadow:_0px_0px_3px_#b0b0b0,_-0px_0px_3px_#b0b0b0,_-0px_0px_3px_#b0b0b0,_0px_-0px_3px_#b0b0b0]'><IconCurrencyRupee className='inline-block' width={16} height={16} stroke={3} />{props.package}</div>
-                <div className='flex items-center gap-1'><IconClockHour3 className='inline-block text-bright-sun-400' />{props.postedDaysAgo} Days ago</div>
+                <div className='text-mine-shaft-200 [text-shadow:_0px_0px_3px_#b0b0b0,_-0px_0px_3px_#b0b0b0,_-0px_0px_3px_#b0b0b0,_0px_-0px_3px_#b0b0b0]'>
+                    <IconCurrencyRupee className='inline-block' width={16} height={16} stroke={3} />
+                    {props.packageOffered + " "}lpa</div>
+                <div className='flex items-center gap-1'><IconClockHour3 className='inline-block text-bright-sun-400' />{formatDateToDayFromCurrentDate(props.postTime)}ago</div>
             </div>
         </Link>
     )
--- src/services/Utilities/Utilities.tsx
+++ src/services/Utilities/Utilities.tsx
@@ -4,4 +4,40 @@ const formatToMonthYear = (dateStr: string): string => {
     if (!dateStr) return 'present'; // Handle empty date string
     return dayjs(dateStr).format('MMMM YYYY'); // "June 2025"
 };
-export { formatToMonthYear };
+
+const formatDateToDayFromCurrentDate = (dateStr: string): string => {
+    if (!dateStr) return '';
+
+    const now = dayjs();
+    const givenDate = dayjs(dateStr);
+
+    const secondsDiff = now.diff(givenDate, 'second');
+    if (secondsDiff < 60) {
+        return `${secondsDiff} second${secondsDiff === 1 ? '' : 's'} `;
+    }
+
+    const minutesDiff = now.diff(givenDate, 'minute');
+    if (minutesDiff < 60) {
+        return `${minutesDiff} minute${minutesDiff === 1 ? '' : 's'} `;
+    }
+
+    const hoursDiff = now.diff(givenDate, 'hour');
+    if (hoursDiff < 24) {
+        return `${hoursDiff} hour${hoursDiff === 1 ? '' : 's'} `;
+    }
+
+    const daysDiff = now.diff(givenDate, 'day');
+    if (daysDiff <= 31) {
+        return `${daysDiff} day${daysDiff === 1 ? '' : 's'}  `;
+    }
+
+    const monthsDiff = now.diff(givenDate, 'month');
+    if (monthsDiff < 12) {
+        return `${monthsDiff} month${monthsDiff === 1 ? '' : 's'} `;
+    }
+
+    const yearsDiff = now.diff(givenDate, 'year');
+    return `${yearsDiff} year${yearsDiff === 1 ? '' : 's'} `;
+};
+
+export { formatToMonthYear, formatDateToDayFromCurrentDate };
