931619ae1d5ef122e566b5e6415dfa6af88d4382
VIPIN YADAV <yadavvipinysy083@gmail.com>
2025-09-08 19:06:26 +0530

    removed node modules

--- src/services/AuthService.tsx
+++ src/services/AuthService.tsx
@@ -6,7 +6,7 @@ const logInUser = async (login: any) => {
     return await axiosInterceptor.post("users/login", login)
         .then(response => response.data)
         .catch(error => {
-            console.error("There was an error logging in the user!", error);
+            console.error("There was an error logging in the user and then error is!", error);
             throw error;
         });
 }
